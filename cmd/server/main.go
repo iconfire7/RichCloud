f4c130af42748fcadada3bb49645b0a734564f0e
Mus-mil <aminsabirov2006@gmail.com>
2025-11-05 19:17:23 +0300

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {})
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module richcloud
+
+go 1.25.1
